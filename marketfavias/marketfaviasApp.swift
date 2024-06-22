bd0255e82e003d380c49b6575ae785252ca9b9d8
Olya Aleshina <exsi112@gmail.com>
2024-06-23 02:13:39 +0300

    Initial Commit

---
+++ marketfavias/marketfaviasApp.swift
@@ -0,0 +1,17 @@
+//
+//  marketfaviasApp.swift
+//  marketfavias
+//
+//  Created by Olya on 23.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct marketfaviasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
